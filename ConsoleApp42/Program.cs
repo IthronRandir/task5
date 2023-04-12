e3cf6ea974204165806403f289722744024e4553
виктор глинко <frtopo@yandex.by>
2023-04-12 16:26:40 +0300

    Добавьте файлы проекта.

---
+++ ConsoleApp42/Program.cs
@@ -0,0 +1,25 @@
+﻿namespace ConsoleApp
+{
+    class Program
+    {
+        static void MaxIndex(int[] numArray)
+        {
+            int max = numArray[0];
+            int index = 0;
+            for (int i = 1; i < numArray.Length; i++)
+            {
+                if (max < numArray[i])
+                {
+                    max = numArray[i];
+                    index = i;
+                }
+            }
+            Console.WriteLine(index);
+        }
+        static void Main(string[] args)
+        {
+            int[] numArr = new int[7] { 1, 5, 8, 9, 4, 3, 2 };
+            MaxIndex(numArr);
+        }
+    }
+}
